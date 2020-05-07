e8cafe31fc29a1a88c53529d0512bbe00b0b0a8f
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2020-05-07 11:10:53 +0530

    initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MapInfoWindow} from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-maps-demo';
+export class AppComponent implements OnInit {
+  @ViewChild(MapInfoWindow, {static: false}) info: MapInfoWindow;
+  zoom = 8;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  };
+  markers = [];
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
+  }
+
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom) {
+      this.zoom++;
+    }
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom) {
+      this.zoom--;
+    }
+  }
+
+  click(event: google.maps.MouseEvent) {
+    console.log(event);
+  }
+
+  addMarker() {
+    this.markers.push({
+      position: {
+        lat: this.center.lat + ((Math.random() - 0.5) * 2) / 10,
+        lng: this.center.lng + ((Math.random() - 0.5) * 2) / 10,
+      },
+      label: {
+        color: 'red',
+        text: 'Marker label ' + (this.markers.length + 1),
+      },
+      title: 'Marker title ' + (this.markers.length + 1),
+      options: {animation: google.maps.Animation.BOUNCE},
+    });
+  }
 }
